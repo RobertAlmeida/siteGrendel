c7c660acd5d741367f3bf5ab0827c5758584a98c
Robert Almeida <robertalmeida1597@gmail.com>
2020-06-18 15:13:29 -0300

    Saved Rep

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res){
+  res.redirect('/');
+});
+
+let port = 3000;
+app.listen(port);
+console.log('App ROdando', port);
