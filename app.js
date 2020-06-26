49a9a8af7e16966e37089e9498b3d8ef4011d4a8
Robert Almeida <robertalmeida1597@gmail.com>
2020-06-26 15:33:13 -0300

    Saved

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ app.use(express.static(__dirname));
 
 app.get('*', function(req, res){
   res.redirect('/');
-  console.log('MAIS OU ACESSO!!!!!!!!!!!!!!')
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log('MAIS OU ACESSO!!!!!!!!!!!!!!');
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log('');
+
 });
 
 let port = process.env.PORT || 3000
